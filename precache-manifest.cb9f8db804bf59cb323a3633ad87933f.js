f4c6ef880a3b2fb6e828045b3fdd2cce3c7c5c47
AlexanderMezhenskyi <alexmezhenskyi@gmail.com>
2019-09-12 00:43:35 +0300

    Updates

---
+++ precache-manifest.cb9f8db804bf59cb323a3633ad87933f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65ceef9af5ec03b6be2e4fbb4cefbd2e",
+    "url": "/countries/index.html"
+  },
+  {
+    "revision": "27bf68845c195edd1823",
+    "url": "/countries/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "8dc030418d27a90fb62c",
+    "url": "/countries/static/js/2.4dfd077f.chunk.js"
+  },
+  {
+    "revision": "27bf68845c195edd1823",
+    "url": "/countries/static/js/main.eb73974e.chunk.js"
+  },
+  {
+    "revision": "0296624c7a05bae4c19a",
+    "url": "/countries/static/js/runtime~main.1d433b0d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/countries/static/media/logo.5d5d9eef.svg"
+  }
+]);
